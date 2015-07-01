2659fadc56d807b30c19a4dd08a921ee4078a92e
HiasminAlves <hiasminalves21@gmail.com>
2015-07-01 20:16:09 -0300

    ShowDoMilhaoHiasminFinal

--- Show do Milhão/ShowdoMilhaoHiasmin/src/dao/RankingDAO.java
+++ Show do Milhão/ShowdoMilhaoHiasmin/src/dao/RankingDAO.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import modelo.Ranking;
-
+ 
 public class RankingDAO {
 
     public Boolean inserir(Ranking ranking) {
